45a438128483cae336c5570b636dffddd9cdee21
Joe Fehrman <joseph.fehrman@gmail.com>
2019-12-26 10:15:41 -0500

    Generator is working no problem.

--- plopfile.js
+++ plopfile.js
@@ -1,30 +1,11 @@
+const { loadSettings } = require('./scripts/utils/loadUtils')
 const {
-  loadSettings,
-  applySettings,
-} = require('./scripts/utils/loadUtils.js')
-const {
-  getFileInfo,
-  getJsFileExtension,
-} = require('./scripts/utils/fileStructureUtils.js')
-const { prompt } = require('./scripts/utils/promptUtils.js')
-const { addAction } = require('./scripts/utils/actionUtils.js')
+  useEmptyReducerGenerator,
+} = require('./scripts/generators/empty-reducer')
 
 const settings = loadSettings()
 
 module.exports = (plop) => {
-  plop.setGenerator('hello world', {
-    prompts: [
-      prompt('input', 'name', 'What is your name?'),
-    ],
-    actions(data) {
-      applySettings(data, settings)
-      return [
-        addAction(
-          'hello_world.txt',
-          'Hello {{name}}!',
-        ),
-      ]
-    },
-  })
+  useEmptyReducerGenerator(plop, settings)
 }
 
---
+++ scripts/generators/empty-reducer.js
@@ -0,0 +1,61 @@
+const appRoot = require('app-root-path')
+const { prompt } = require('../utils/promptUtils')
+const { applySettings } = require('../utils/loadUtils')
+const { addAction, modifyAction } = require('../utils/actionUtils')
+const { emptyReducer } = require('../../plop-templates/templates')
+
+const generateEmptyReducerActions = ({
+  isSemicolons,
+  jsExt,
+}) => {
+  let actions = [
+    addAction(
+      `${appRoot.path}/src/redux/reducer/{{snakeCase name}}.${jsExt}`,
+      emptyReducer,
+    ),
+    addAction(
+      `${appRoot.path}/src/redux/creator/{{snakeCase name}}.${jsExt}`,
+      '',
+    ),
+    addAction(
+      `${appRoot.path}/src/redux/actions/{{snakeCase name}}.${jsExt}`,
+      '',
+    ),
+  ]
+
+  if (!isSemicolons) {
+    actions = [
+      ...actions,
+      modifyAction(
+        `${appRoot.path}/src/redux/reducer/{{snakeCase name}}.${jsExt}`,
+        /;\n/g,
+        '\n',
+      ),
+    ]
+  }
+
+  return actions
+}
+
+const useEmptyReducerGenerator = (plop, settings) => {
+  plop.setGenerator('empty reducer', {
+    description: 'Create an empty skeleton reducer.',
+    prompts: [
+      prompt(
+        'input',
+        'name',
+        'What is the name of the reducer?',
+      ),
+    ],
+    actions(data) {
+      applySettings(data, settings)
+      return generateEmptyReducerActions(data)
+    },
+  })
+}
+
+module.exports = {
+  useEmptyReducerGenerator,
+  generateEmptyReducerActions,
+}
+
